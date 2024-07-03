47c1b05915e652718f8bca26832dc613e0fd24b2
Mark Anton Mamac <mgmamac17@gmail.com>
2024-07-04 02:08:24 +0800

    Fix path for launching Roblox.

--- Roblox Launcher.py
+++ Roblox Launcher.py
@@ -1,6 +1,6 @@
 import os
 
-verList = os.listdir("..\\Program Files (x86)\\Roblox\\Versions")
+verList = os.listdir("C:\\Program Files (x86)\\Roblox\\Versions")
 
 path = ""
 
@@ -8,7 +8,7 @@ for i in verList:
     prefix = i.split("-")[0]
     
     if prefix == "version":
-        path = "..\\Program Files (x86)\\Roblox\\Versions\\" + i
+        path = "C:\\Program Files (x86)\\Roblox\\Versions\\" + i
         
         if "RobloxPlayerBeta.exe" in os.listdir(path):
             break
