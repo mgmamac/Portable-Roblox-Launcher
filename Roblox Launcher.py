ac08a940f52ae40aeb36060f5b4c12e9aceb5e69
Mark Anton Mamac <mgmamac17@gmail.com>
2026-06-27 16:12:32 +0800

    Removal of older version

--- Roblox Launcher.py
+++ Roblox Launcher.py
@@ -1,5 +1,36 @@
-import os
+#compile with pyinstaller "Roblox Launcher.exe" --onefile --noconsole
 
+import os
+import shutil
+
+verList = os.listdir("C:\\Program Files (x86)\\Roblox\\Versions")
+
+path = ""
+
+latestModified = 0.0
+
+#check for older versions
+for i in verList:
+    prefix = i.split("-")[0]
+    
+    if prefix == "version":
+        path = "C:\\Program Files (x86)\\Roblox\\Versions\\" + i
+        
+        if(os.path.getmtime(path) >= latestModified):
+            latestModified = os.path.getmtime(path)
+
+#remove older versions
+for i in verList:
+    prefix = i.split("-")[0]
+    
+    if prefix == "version":
+        path = "C:\\Program Files (x86)\\Roblox\\Versions\\" + i
+        
+        if(os.path.getmtime(path) != latestModified):
+            shutil.rmtree(path)
+        
+
+#run roblox
 verList = os.listdir("C:\\Program Files (x86)\\Roblox\\Versions")
 
 path = ""
@@ -9,9 +40,10 @@ for i in verList:
     
     if prefix == "version":
         path = "C:\\Program Files (x86)\\Roblox\\Versions\\" + i
-        
+
         if "RobloxPlayerBeta.exe" in os.listdir(path):
             break
 
-os.chdir(path)
-os.system("start RobloxPlayerBeta.exe")
+if(path != ""):
+    os.chdir(path)
+    os.system("start RobloxPlayerBeta.exe")
